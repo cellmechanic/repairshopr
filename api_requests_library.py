c31d7d331a892ee78e22c99bd50c6d9ce30d76d8
justin millman <support@cellmechanic.com>
2023-08-02 16:49:58 -0400

    changed time comparison to unix for int usage

--- api_requests_library.py
+++ api_requests_library.py
@@ -1,4 +1,5 @@
 """library for diff API requests"""
+import time
 import requests
 import env_library  # Make sure that you import the necessary variables from env_library
 
@@ -7,10 +8,47 @@ def get_contacts(page):
     """api request"""
     url = f'{env_library.api_url_contact}?page={page}'
     headers = {'Authorization': f'Bearer {env_library.api_key_contact}'}
-    # Assuming the headers are defined in env_library
-    response = requests.get(url, headers=headers, timeout=10)
-    if response.status_code != 200:
-        print(f'Error fetching contacts on page {page}: {response.text}')
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        if response.status_code != 200:
+            print(f'Error fetching contacts on page {page}: {response.text}')
+            return None
+        return response.json()
+    except requests.RequestException as error:
+        print(f'Failed to get data for page {page}: {str(error)}')
         return None
 
-    return response.json()
+
+def update_last_ran(timestamp_file):
+    ''' update the last time the script ran file, pass in the file'''
+    try:
+        with open(timestamp_file, 'w', encoding='utf-8') as file:
+            file.write(str(int(time.time())))
+        print("Timestamp updated successfully to: ", int(time.time()))
+    except IOError as error:
+        print(f"Error updating timestamp: {error}")
+
+
+def check_last_ran(timestamp_file):
+    ''' check the passed timestamp file to see the last time this ran'''
+    try:
+        with open(timestamp_file, 'r', encoding='utf-8') as file:
+            last_run_timestamp_str = file.read().strip()
+            if last_run_timestamp_str:
+                last_run_timestamp = int(last_run_timestamp_str)
+                print(f"Last ran @ {last_run_timestamp_str} in unix")
+                last_run_time_str = time.strftime('%Y-%m-%d %H:%M:%S',
+                                                  time.localtime(last_run_timestamp))
+                print(f"Last ran @ {last_run_time_str}")
+                return int(last_run_timestamp_str)
+            else:
+                print("no timestamp found, using now as time")
+                return int(time.time())
+    except FileNotFoundError:
+        print("no timestamp file found, using now as time")
+        return int(time.time())
+
+
+def get_timestamp_code():
+    '''return the correct timestamp code to stay uniform'''
+    return '%Y-%m-%dT%H:%M:%S.%f%z'
--- contacts_to_db.py
+++ contacts_to_db.py
@@ -1,114 +1,160 @@
 """Getting RS Contacts"""
+from datetime import datetime
 import mysql.connector
 import env_library
 from fix_dates_library import format_date_fordb
 from api_requests_library import get_contacts
+from api_requests_library import update_last_ran
+from api_requests_library import check_last_ran
+from api_requests_library import get_timestamp_code
+
+# Load timestamp
+TIMESTAMP_FILE = 'last_run_contacts.txt'
+last_run_timestamp_unix = check_last_ran(TIMESTAMP_FILE)
 
 # Database configuration
 config = env_library.config
+CONNECTION = None
 
 # Connect to the database
-connection = mysql.connector.connect(**config)
-cursor = connection.cursor()
+try:
+    CONNECTION = mysql.connector.connect(**config)
+    cursor = CONNECTION.cursor()
 
-# Create the contacts table if not exists
-cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    address1 VARCHAR(255),
-    address2 VARCHAR(255),
-    city VARCHAR(255),
-    state VARCHAR(255),
-    zip VARCHAR(255),
-    email VARCHAR(255),
-    phone VARCHAR(255),
-    mobile VARCHAR(255),
-    latitude FLOAT,
-    longitude FLOAT,
-    customer_id INT,
-    account_id INT,
-    notes TEXT,
-    created_at DATETIME,
-    updated_at DATETIME,
-    vendor_id INT,
-    title VARCHAR(255),
-    opt_out BOOLEAN,
-    extension VARCHAR(50),
-    processed_phone VARCHAR(255),
-    processed_mobile VARCHAR(255),
-    ticket_matching_emails VARCHAR(255)
-)
-''')
+    # Create the contacts table if not exists
+    cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (
+            id INT PRIMARY KEY,
+            name VARCHAR(255),
+            address1 VARCHAR(255),
+            address2 VARCHAR(255),
+            city VARCHAR(255),
+            state VARCHAR(255),
+            zip VARCHAR(255),
+            email VARCHAR(255),
+            phone VARCHAR(255),
+            mobile VARCHAR(255),
+            latitude FLOAT,
+            longitude FLOAT,
+            customer_id INT,
+            account_id INT,
+            notes TEXT,
+            created_at DATETIME,
+            updated_at DATETIME,
+            vendor_id INT,
+            title VARCHAR(255),
+            opt_out BOOLEAN,
+            extension VARCHAR(50),
+            processed_phone VARCHAR(255),
+            processed_mobile VARCHAR(255),
+            ticket_matching_emails VARCHAR(255)
+        )
+        ''')
+    # Fetch data from the API
+    headers = {'Authorization': f'Bearer {env_library.api_key_contact}'}
 
-# Fetch data from the API
-headers = {'Authorization': f'Bearer {env_library.api_key_contact}'}
+    # Start fetching contacts from page 1
+    PAGE = 1
+    TOTAL_PAGES = 0
+    TOTAL_ENTRIES = 0
+    ENTRY_COUNT = 0
+    DB_ROWS = 0
 
-# Start fetching contacts from page 1
-PAGE = 1
-data = get_contacts(PAGE)
-total_pages = data['meta']['total_pages']
-total_entries = data['meta']['total_entries']
-ENTRY_COUNT = 0
+    data = get_contacts(PAGE)
+    if data is not None:
+        TOTAL_PAGES = data['meta']['total_pages']
+        TOTAL_ENTRIES = data['meta']['total_entries']
+    else:
+        print("Error getting contact data")
 
-# Iterate through the pages
-# Iterate through the pages
-while PAGE <= total_pages:
-    for contact in data['contacts']:
-        # fix dates for the DB
-        created_at_str = contact['created_at']
-        formatted_created_at = format_date_fordb(created_at_str)
-        updated_at_str = contact['updated_at']
-        formatted_updated_at = format_date_fordb(updated_at_str)
+        # Iterate through the pages
+    while PAGE <= TOTAL_PAGES:
+        if data is not None:
+            for contact in data['contacts']:
+                # fix dates for the DB
+                created_at_str = contact['created_at']
+                formatted_created_at = format_date_fordb(created_at_str)
+                updated_at_str = contact['updated_at']
+                updated_at_unix = int(datetime.strptime(
+                    updated_at_str, get_timestamp_code()).timestamp())
+                formatted_updated_at = format_date_fordb(updated_at_str)
 
-        cursor.execute('''
-        INSERT INTO contacts (id, name, address1, address2, city, state, zip, email, phone, mobile, latitude, longitude, customer_id, 
-                       account_id, notes, created_at, updated_at, vendor_id, title, opt_out, extension, processed_phone, processed_mobile, ticket_matching_emails)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                       ON DUPLICATE KEY UPDATE
-    name = VALUES(name),
-    address1 = VALUES(address1),
-    address2 = VALUES(address2),
-    city = VALUES(city),
-    state = VALUES(state),
-    zip = VALUES(zip),
-    email = VALUES(email),
-    phone = VALUES(phone),
-    mobile = VALUES(mobile),
-    latitude = VALUES(latitude),
-    longitude = VALUES(longitude),
-    customer_id = VALUES(customer_id),
-    account_id = VALUES(account_id),
-    notes = VALUES(notes),
-    created_at = VALUES(created_at),
-    updated_at = VALUES(updated_at),
-    vendor_id = VALUES(vendor_id),
-    title = VALUES(title),
-    opt_out = VALUES(opt_out),
-    extension = VALUES(extension),
-    processed_phone = VALUES(processed_phone),
-    processed_mobile = VALUES(processed_mobile),
-    ticket_matching_emails = VALUES(ticket_matching_emails)
-        ''', (
-            contact['id'], contact['name'], contact['address1'], contact['address2'],
-            contact['city'], contact['state'], contact['zip'], contact['email'],
-            contact['phone'], contact['mobile'], contact['latitude'], contact['longitude'],
-            contact['customer_id'], contact['account_id'], contact['notes'],
-            formatted_created_at, formatted_updated_at, contact['vendor_id'],
-            contact['properties']['title'] if 'title' in contact['properties'] else None,
-            contact['opt_out'], contact['extension'], contact['processed_phone'],
-            contact['processed_mobile'], contact['ticket_matching_emails']
-        ))
-        connection.commit()
-        ENTRY_COUNT += 1
+                if updated_at_unix > last_run_timestamp_unix:
+                    print(
+                        f"Contact {contact['id']} has been updated since last run.")
+                    cursor.execute('''
+                        INSERT INTO contacts (id, name, address1, address2, city, state, zip, email, phone, mobile, latitude, longitude, customer_id, 
+                                account_id, notes, created_at, updated_at, vendor_id, title, opt_out, extension, processed_phone, 
+                                processed_mobile, ticket_matching_emails)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                                ON DUPLICATE KEY UPDATE
+                        name = VALUES(name),
+                        address1 = VALUES(address1),
+                        address2 = VALUES(address2),
+                        city = VALUES(city),
+                        state = VALUES(state),
+                        zip = VALUES(zip),
+                        email = VALUES(email),
+                        phone = VALUES(phone),
+                        mobile = VALUES(mobile),
+                        latitude = VALUES(latitude),
+                        longitude = VALUES(longitude),
+                        customer_id = VALUES(customer_id),
+                        account_id = VALUES(account_id),
+                        notes = VALUES(notes),
+                        created_at = VALUES(created_at),
+                        updated_at = VALUES(updated_at),
+                        vendor_id = VALUES(vendor_id),
+                        title = VALUES(title),
+                        opt_out = VALUES(opt_out),
+                        extension = VALUES(extension),
+                        processed_phone = VALUES(processed_phone),
+                        processed_mobile = VALUES(processed_mobile),
+                        ticket_matching_emails = VALUES(ticket_matching_emails)
+                        ''', (
+                        contact['id'], contact['name'], contact['address1'], contact['address2'],
+                        contact['city'], contact['state'], contact['zip'], contact['email'],
+                        contact['phone'], contact['mobile'], contact['latitude'],
+                        contact['longitude'], contact['customer_id'],
+                        contact['account_id'], contact['notes'],
+                        formatted_created_at, formatted_updated_at, contact['vendor_id'],
+                        contact['properties']['title'] if 'title' in contact['properties']
+                        else None,
+                        contact['opt_out'], contact['extension'], contact['processed_phone'],
+                        contact['processed_mobile'], contact['ticket_matching_emails']
+                    ))
+                    CONNECTION.commit()
+                    ENTRY_COUNT += 1
 
-    print(f'Page {PAGE} / {total_pages} processed.')
-    PAGE += 1
-    if PAGE <= total_pages:
-        data = get_contacts(PAGE)
+            print(f'Page {PAGE} / {TOTAL_PAGES} processed.')
+            PAGE += 1
+            if PAGE <= TOTAL_PAGES:
+                data = get_contacts(PAGE)
+                if data is None:
+                    print(f"Error getting data on {PAGE}")
+                    break
+            else:
+                print(f"All data received from {TOTAL_PAGES} page(s)")
+                QUERY = "SELECT COUNT(*) FROM contacts"
+                cursor.execute(QUERY)
+                result = cursor.fetchone()
+                if result is not None:
+                    DB_ROWS = result[0]
+                break
 
-# Check if the total entries match the expected count
-if ENTRY_COUNT != total_entries:
-    print(
-        f'Warning: Expected {total_entries} entries but found {ENTRY_COUNT}.')
+        # Check if the total entries match the expected count
+    if ENTRY_COUNT != TOTAL_ENTRIES:
+        print(
+            f'Warning: Made changes to {ENTRY_COUNT} entries but found, '
+            f'Meta Rows: {TOTAL_ENTRIES}.')
+        print(f'Row Count from DB is: {DB_ROWS}')
 
-print("Contacts successfully inserted into the database.")
+    print("Contacts successfully inserted into the database.")
+    print(f"Made: {TOTAL_ENTRIES} updates")
+except mysql.connector.Error as err:
+    print(f"Database error {err}")
+finally:
+    if CONNECTION and CONNECTION.is_connected():
+        CONNECTION.close()
+        update_last_ran(TIMESTAMP_FILE)
+
+print("Database connection closed.")
