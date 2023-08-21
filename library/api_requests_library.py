81e3306352df97a0700405feb03eab0d71f22439
justin millman <support@cellmechanic.com>
2023-08-21 18:04:36 -0400

    pycharm

--- library/api_requests_library.py
+++ library/api_requests_library.py
@@ -1,5 +1,7 @@
 """api requests"""
+
 import requests
+from datetime import datetime, timedelta
 from library import env_library
 from library.fix_date_time_library import log_ts
 
@@ -34,12 +36,22 @@ def get_invoice_lines(page):
         return None
 
 
-def get_tickets(page):
+def get_tickets(page=None, look_back_period=None):
     """api request"""
-    url = f"{env_library.api_url_tickets}?page={page}"
+
+    # base url / header key
+    url = f"{env_library.api_url_tickets}"
     headers = {"Authorization": f"Bearer {env_library.api_key_tickets}"}
+
+    # params
+    params = {}
+    if page is not None:
+        params["page"] = page
+    if look_back_period is not None:
+        params["since_updated_at"] = get_date_for_header(look_back_period)
+
     try:
-        response = requests.get(url, headers=headers, timeout=10)
+        response = requests.get(url, headers=headers, params=params, timeout=10)
         if response.status_code != 200:
             print(f"{log_ts()} Error fetching invoice line items on page {page}: {response.text}")
             return None
@@ -48,3 +60,8 @@ def get_tickets(page):
     except requests.RequestException as error:
         print(f"{log_ts()} Failed to get data for page {page}: {str(error)}")
         return None
+
+
+def get_date_for_header(look_back):
+    date_before = datetime.now() - timedelta(days=look_back)
+    return date_before.strftime('%Y-%m-%d')
