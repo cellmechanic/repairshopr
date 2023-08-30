b50e8724da70747875b3e783c9e96471c804e7e1
justin millman <support@cellmechanic.com>
2023-08-30 18:00:32 -0400

    merge

--- main_frequent.py
+++ main_frequent.py
@@ -1,14 +1,6 @@
 """ main script for the frequent modules """
-<<<<<<< HEAD
-from modules.contacts import contacts
-
-contacts()
-
-
-=======
 from modules.invoice_lines_update import invoice_lines_update
 from modules.contacts import contacts
 
 contacts()
 invoice_lines_update()
->>>>>>> testing
