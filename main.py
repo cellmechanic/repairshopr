5d0eb612bd468e6b8953f525ac739595ec9cfda1
justin millman <support@cellmechanic.com>
2023-10-12 16:42:08 -0400

    comnents

--- main.py
+++ main.py
@@ -37,14 +37,14 @@ def main():
             extra={"tags": {"service": "main_frequent", "finished": "yes"}},
         )
 
-        # volume()
-        # contacts(logger, False)
-        # customers(logger, False)
-        estimates(logger, False, 7) #
+        volume()
+        contacts(logger, False)
+        customers(logger, False)
+        estimates(logger, False, 7)  #
         invoice_lines(logger, False)
-        invoices(logger, False, 7) # has since_updated_at
+        invoices(logger, False, 7)  # has since_updated_at
         payments(logger, False, 7)
-        tickets(logger, False, 14) # has since_updated_at
+        tickets(logger, False, 14)  # has since_updated_at
         products(logger, False)
         users(logger)
         output(logger)
@@ -62,14 +62,14 @@ def main():
             extra={"tags": {"service": "main_full", "finished": "full"}},
         )
 
-        # backup_database(logger)
-        # upload_to_drive(logger)
-        # contacts(logger, True)
-        # customers(logger, True)
-        # estimates(logger, True)
-        # invoice_lines(logger, True)
-        # invoices(logger, True)
-        # payments(logger, True)
+        backup_database(logger)
+        upload_to_drive(logger)
+        contacts(logger, True)
+        customers(logger, True)
+        estimates(logger, True)
+        invoice_lines(logger, True)
+        invoices(logger, True)
+        payments(logger, True)
         tickets(logger, True)
         products(logger, True)
 
