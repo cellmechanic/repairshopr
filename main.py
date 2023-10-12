37eb9a95e933ce1d9e7673254accda50af2143c3
justin millman <support@cellmechanic.com>
2023-10-12 14:33:58 -0400

    testing products issue

--- main.py
+++ main.py
@@ -62,15 +62,15 @@ def main():
             extra={"tags": {"service": "main_full", "finished": "full"}},
         )
 
-        backup_database(logger)
-        upload_to_drive(logger)
-        # contacts(logger, True)
-        # customers(logger, True)
-        # estimates(logger, True)
-        # invoice_lines(logger, True)
-        # invoices(logger, True)
-        # payments(logger, True)
-        # tickets(logger, True)
+        # backup_database(logger)
+        # upload_to_drive(logger)
+        contacts(logger, True)
+        customers(logger, True)
+        estimates(logger, True)
+        invoice_lines(logger, True)
+        invoices(logger, True)
+        payments(logger, True)
+        tickets(logger, True)
         # products(logger, True)
 
         logger.info(
